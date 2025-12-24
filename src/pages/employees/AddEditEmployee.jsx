b13ea41b799f84db6166603eaeedf40c5144d828
vaibhavkadusAL <vaibhavkadus@alphaseam.com>
2025-12-24 15:45:19 +0530

    project stucture

---
+++ src/pages/employees/AddEditEmployee.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddEditEmployee() {
+  return (
+    <div>AddEditEmployee</div>
+  )
+}
+
+export default AddEditEmployee
---
+++ src/pages/products/AddEditProduct.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddEditProduct() {
+  return (
+    <div>AddEditProduct</div>
+  )
+}
+
+export default AddEditProduct
